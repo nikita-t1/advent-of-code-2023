f4bcf14af9ab59a55e5bb042b6da7a847c66f329
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2023-12-02 11:40:57 +0100

    Day 02 - Part2

--- src/Day02.kt
+++ src/Day02.kt
@@ -40,12 +40,48 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        val condition = mapOf(
+                "red" to 12,
+                "green" to 13,
+                "blue" to 14
+        )
+        for ((index, line) in input.withIndex()) {
+            // remove game id
+            // 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+            val s = line.substringAfter(":")
+
+            // split into sets
+            // 3 blue, 4 red
+            val sets = s.split(";")
+            println("sets: ${sets.map {  "$it | " }}")
+
+            var green = 0
+            var red = 0
+            var blue = 0
+            for (set in sets) {
+                val cubes = set.split(",")
+                cubes.forEach {
+                    val split = it.trim().split(" ")
+                    val amount = split[0].toInt()
+                    val color = split[1]
+                    println("amount: $amount, color: $color")
+                    if (color == "green" && green < amount) green = amount
+                    if (color == "red" && red < amount) red = amount
+                    if (color == "blue" && blue < amount) blue = amount
+                }
+
+            }
+            sum += (green * red * blue)
+        }
+
+        return sum
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
     check(part1(testInput) == 8)
+    check(part2(testInput) == 2286)
 
     val input = readInput("Day02")
     part1(input).println()
